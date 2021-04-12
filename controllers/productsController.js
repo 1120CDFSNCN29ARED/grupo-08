c3333ff3bc03ba0bd9fb75996007705d3c9bb13d
--unset <eugeniahermosilla@gmail.com>
2021-04-11 22:59:10 -0300

    add new product routes

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,14 +1,101 @@
+const { validationResult } = require("express-validator");
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
+const path = require("path");
+
 const productsController = {
   showproductsView: (req, res) => {
-    const productId = req.params.productId;
+    // Leer y traducir la base de datos
+    const productsListFile = fs.readFileSync(
+      path.join(__dirname, "../data/products.json"),
+      { encoding: "utf8" }
+    );
+    const products = JSON.parse(productsListFile);
 
-    res.render("products/product", { productId: productId });
+    // Buscar nuestro objeto por su id
+    const product = products.find(
+      (product) => product.id === req.params.productId
+    );
+
+    // Compartirlo a la vista
+    res.render("products/product", { product: product });
   },
   showNewProductView: (req, res) => {
     res.render("products/new-product");
   },
   showEditProductView: (req, res) => {
     res.render("products/edit-product");
+  },
+  createNewProduct: (req, res) => {
+    const errors = validationResult(req);
+
+    if (errors.isEmpty()) {
+      // Create new product object
+      const newProduct = {
+        id: uuidv4(),
+        name: req.body.name,
+        description: req.body.description,
+        category: req.body.category,
+        colors: req.body.colors,
+        price: req.body.price,
+        image: req.file.filename,
+      };
+
+      // Read products from JSON
+      const productsListFile = fs.readFileSync(
+        path.join(__dirname, "../data/products.json"),
+        { encoding: "utf8" }
+      );
+
+      let products;
+
+      // Check if file is empty
+      if (productsListFile === "") {
+        products = [];
+      } else {
+        products = JSON.parse(productsListFile);
+      }
+
+      // Add new product to array
+      products.push(newProduct);
+
+      // Convert object into string
+      const productsJSON = JSON.stringify(products);
+
+      // Write json file
+      fs.writeFileSync(
+        path.join(__dirname, "../data/products.json"),
+        productsJSON
+      );
+
+      res.render("sucess-product", { productTitle: req.body.name });
+    } else {
+      res.render("products/new-product", {
+        errors: errors.mapped(),
+        old: req.body,
+      });
+    }
+  },
+  showProductList: (req, res) => {
+    const productsListFile = fs.readFileSync(
+      path.join(__dirname, "../data/products.json"),
+      { encoding: "utf8" }
+    );
+    const products = JSON.parse(productsListFile);
+
+    res.render("products/products", { products: products });
+  },
+  showProductListByCategory: (req, res) => {
+    const productsListFile = fs.readFileSync(
+      path.join(__dirname, "../data/products.json"),
+      { encoding: "utf8" }
+    );
+    const products = JSON.parse(productsListFile);
+    const filtedProduct = products.filter(
+      (product) => product.category === req.params.category
+    );
+
+    res.render("products/products", { products: filtedProduct });
   },
 };
 
--- routes/products.js
+++ routes/products.js
@@ -1,17 +1,81 @@
 const express = require("express");
+const { check } = require("express-validator");
 const router = express.Router();
 const productsController = require("../controllers/productsController");
+const strings = require("../constants/strings");
+const multer = require("multer");
+const { v4: uuidv4 } = require("uuid");
+const path = require("path");
 
-router.get("/new-product", function (req, res) {
+// Multer config
+const multerDiskStorage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    const folder = path.join(__dirname, "../public/images/products");
+    callback(null, folder);
+  },
+  filename: (req, file, callback) => {
+    const imageName = uuidv4() + path.extname(file.originalname);
+    callback(null, imageName);
+  },
+});
+
+const fileUpload = multer({ storage: multerDiskStorage });
+
+// Express validator
+const validateCreateProduct = [
+  check("name")
+    .notEmpty()
+    .withMessage(strings.VALIDATE_CREATE_PRODUCT_NAME_EMPTY)
+    .isLength({
+      min: 3,
+      max: 45,
+    })
+    .withMessage(strings.VALIDATE_CREATE_PRODUCT_NAME_LENGTH),
+  check("description")
+    .notEmpty()
+    .withMessage(strings.VALIDATE_CREATE_PRODUCT_DESCRIPTION_EMPTY)
+    .isLength({
+      min: 3,
+      max: 500,
+    })
+    .withMessage(strings.VALIDATE_CREATE_PRODUCT_DESCRIPTION_LENGTH),
+  check("category")
+    .notEmpty()
+    .withMessage(strings.VALIDATE_CREATE_PRODUCT_CATEGORY_EMPTY),
+  check("colors")
+    .notEmpty()
+    .withMessage(strings.VALIDATE_CREATE_PRODUCT_COLOR_EMPTY),
+  check("price")
+    .notEmpty()
+    .withMessage(strings.VALIDATE_CREATE_PRODUCT_PRICE_EMPTY),
+];
+
+router.get("/create", function (req, res) {
   productsController.showNewProductView(req, res);
 });
 
-router.get("/edit-product", function (req, res) {
+router.get("/edit", function (req, res) {
   productsController.showEditProductView(req, res);
 });
 
 router.get("/:productId", function (req, res) {
   productsController.showproductsView(req, res);
+});
+
+router.post(
+  "/",
+  [fileUpload.single("image"), validateCreateProduct],
+  function (req, res) {
+    productsController.createNewProduct(req, res);
+  }
+);
+
+router.get("/", function (req, res) {
+  productsController.showProductList(req, res);
+});
+
+router.get("/categories/:category", function (req, res) {
+  productsController.showProductListByCategory(req, res);
 });
 
 module.exports = router;
